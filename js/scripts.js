79aa57934ba62d382af2a4bc7df1c3271c639909
Jeremy Smith <smithjeremym@gmail.com>
2016-02-10 18:22:52 -0500

    get mouseover/out colors working, toggle display values to none/inline-block

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,56 @@
 
 var sectionJS = document.querySelector(".titleBox");
 var arrowColorShift = document.querySelector(".icon");
-
-sectionJS.addEventListener("mouseover", function () {
-  arrowColorShift.style = "color:#003A6C";//changes the color of the arrow??
-});
-
-var showText = document.querySelector(".body");
+var showHideText = document.querySelector(".body");
 
 sectionJS.addEventListener("click", function () {
-  showText.style = "display:inline-block";//shows the paragraph upon a click of the section
+  if (showHideText.style.display === ""){
+    showHideText.style.display = "inline-block";//shows the text
+    arrowColorShift.style.color = "#003A6C";
+    }
+  else if (showHideText.style.display === "inline-block"){
+    showHideText.style.display = "";// hides the text
+    arrowColorShift.style.color = "#9B8347";
+  }
 });
 
-// #003A6C
+sectionJS.addEventListener("mouseover", function () {
+  arrowColorShift.style.color = "#003A6C";
+});
+
+sectionJS.addEventListener("mouseout", function () {
+  arrowColorShift.style.color = "#9B8347"; //changes back to gold
+});
+//this didn't work - mouseover doesn't have an 'end'
+// sectionJS.addEventListener("mouseover", function () {
+//   if (arrowColorShift.style.color === "#003A6C"){
+//     arrowColorShift.style.color = "#9B8347";//changes the color of the arrow upon a mouseover
+//   } else if (arrowColorShift.style.color === "#9B8347") {
+//     arrowColorShift.style.color = "#003A6C";
+//   }
+// });
+
+
+// this was an attempt to put a for loop inside of my eventListener. It didn't make much sense
+//
+// sectionJS.addEventListener("click", function () {
+//   for (clickCount = 1; clickCount <= 1000; clickCount++){
+//     if (clickCount % 2 === 0) {
+//       showHideText.style = "display:inline-block";//shows the paragraph upon a click of the section
+//     }
+//     else {
+//       showHideText.style = "display:none";//hides the paragraph
+//     }
+//   }
+//
+// });
+
+//this is an attempt to use onclick. It didn't work
+// document.querySelector(".body").onclick = function() {
+//     style = "display:inline-block";
+// }
+
+
+// sectionJS.addEventListener("click", function () {
+//   hideText.style = "display:none";//hides??? the paragraph upon a click of the section
+// }, false);
